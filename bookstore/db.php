6a90ce78420e8f97acbd292cf3f403fbab13990c
Nopphagaw-T <nopphagaw.t@fitm.kmutnb.ac.th>
2021-03-09 12:01:49 +0700

    create user for access db and create connect to db

---
+++ bookstore/db.php
@@ -0,0 +1,18 @@
+<?php
+    define("hostname","localhost");
+    define("user","user");
+    define("password", "VHiToiT8PmmWbaqn");
+    define("dbname","bookstore");
+?>
+
+<?php
+    $mysqli = new mysqli(hostname, user, password, dbname);
+    // Check connection
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }else{
+        echo "Connect success...";
+    }
+    $sqltxt = ""
+?>
